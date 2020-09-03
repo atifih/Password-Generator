5d21beffd2e8c58fd9995014b97858beef32f992
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-09-03 13:11:04 +1000

    Changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,159 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function passwordOptions(){
+  // Prompt the user for the length of  the password requested.
+  var length = prompt("What length should the password be?");
+
+  // Validate user input for  the ddsired password length.
+  // Convert length to integer so mathematical comparison may proceed.
+var testLength = parseInt(length, 10);
+console.log(testLength);
+console.log(length);
+
+
+ if (testLength < 8 || testLength > 128)
+ // input is invalid.
+  return;
+else {
+
+  // length  is valid)
+ 
+
+  // Query the user for their criteria.
+var upperCharacter = confirm("Do you wish to include uppercase ccharacters?");
+var lowerCharacter = confirm("Do you wish to include lowercase characters?");
+var numberCharacter = confirm("Do you wish to include numeric characters?");
+var specialCharacter = confirm ("Do you wish to include special charscters?");
+// Validate user input.
+if (!upperCharacter && !lowerCharacter && !numberCharacter && !specialCharacter){
+  return; // User must select atleast one password option.
+}
+
+
+// Build a passwordOptions object for the purpose of benerating a password.
+var passwordOptions = {
+    pLength: testLength,
+    upper: upperCharacter,
+    lower: lowerCharacter,
+    number: numberCharacter,
+    special: specialCharacter
+
+  }
+
+
+  
+// return the passwordOptions object for generating a password.
+console.log("Password length is:", passwordOptions.pLength);
+console.log("Special Char selected?",  passwordOptions.special);
+return passwordOptions;
+} // end-else
+}
+
+
+
+function generatePassword(){
+  // Retrieve  the passwordOptions object 
+  //var userOptions = {};
+  
+  var userOptions = passwordOptions();
+
+  var passwordArray = [];
+ //  var concatenatedArray=[];
+ var tempArray1 = [];
+ var tempArray2 = [];
+ var tempArray = [];
+var  potentialchars = [];
+ if (userOptions.upper){
+  // Append generated uppercase character to end of generated password.
+     console.log(upperCasedCharacters);
+
+      // var randIndex = Math.floor(Math.random() * upperCasedCharacters.length);
+      potentialchars =  upperCasedCharacters;
+   
+   if  (userOptions.lower){
+   // Append generated lowercase character to end of generated password.
+     
+    potentialchars += lowerCasedCharacters;
+}
+
+   if  (userOptions.number){
+  // Append generated number character to end of generated password.
+  potentialchars += numericCharacters;
+}
+  if (userOptions.special){
+  //Append generated number character to end of generated password.
+ potentialChars  += specialCharacters;
+}
+
+
+/*
+if (userOptions.upper && userOptions.lower){
+  
+  tempArray1  += upperCasedCharacters[(Math.floor(Math.random() * upperCasedCharacters.length))];
+  tempArray2  += lowerCasedCharacters[(Math.floor(Math.random() * lowerCasedCharacters.length))];
+  tempArray = tempArray1 + tempArray2;
+  passwordArray += tempArray[(Math.floor(Math.random() * tempArray.length))];
+}
+*/
+
+// Loop the array
+  for (var i = 0; i < userOptions.pLength; i++){
+   
+    console.log("password length in for loop: pLength = ", userOptions.pLength);
+   
+    /*
+      if (userOptions.upper){
+      // Append generated uppercase character to end of generated password.
+         console.log(upperCasedCharacters);
+  
+          // var randIndex = Math.floor(Math.random() * upperCasedCharacters.length);
+          passwordArray  += upperCasedCharacters[(Math.floor(Math.random() * upperCasedCharacters.length))];
+        
+       
+       if  (userOptions.lower){
+       // Append generated lowercase character to end of generated password.
+      passwordArray  += lowerCasedCharacters[(Math.floor(Math.random() * lowerCasedCharacters.length))];
+       
+    }
+    
+    if  (userOptions.number){
+      // Append generated number character to end of generated password.
+      passwordArray += numericCharacters[(Math.floor(Math.random() * numericCharacters.length))];
+    }
+     if (userOptions.special){
+      //Append generated number character to end of generated password.
+     passwordArray += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+      console.log("Password (special) is : ", passwordArray);
+    }
+   /*
+    if (userOptions.upper && userOptions.lower){
+      
+      tempArray1  += upperCasedCharacters[(Math.floor(Math.random() * upperCasedCharacters.length))];
+      tempArray2  += lowerCasedCharacters[(Math.floor(Math.random() * lowerCasedCharacters.length))];
+      tempArray = tempArray1 + tempArray2;
+      passwordArray += tempArray[(Math.floor(Math.random() * tempArray.length))];
+    }
+    */
+
+
+   passwordArray += potentialchars[(Math.floor(Math.random() * potentialchars.length))];
+
+
+      
+  console.log("For loop counter var:", i);
+  
+  } // end-for loop
+    console.log("Password generated is: ", passwordArray);
+    //alert (passwordArray);
+   
+   return passwordArray;
+}
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +165,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
